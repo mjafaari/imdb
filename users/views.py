bba1a39c403a2e2c17e6af625d23ef53c2542bf8
mjafaari <mjafaari99@gmail.com>
2022-03-10 22:11:21 +0330

    User APIs added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from django.http import Http404
+from .permissions import IsOwnerOrReadOnly
+from .models import User
+from .serializers import CreateUserSerializer, GetUsersSerializer
 
-# Create your views here.
+
+class UsersHandler(APIView):
+    permission_classes = []
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = GetUsersSerializer(instance=users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CreateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserHandler(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    def get_user(self, pk):
+        user = User.objects.get(email=pk)
+        try:
+            return user
+        except User.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk, format=None):
+        user = self.get_user(pk)
+        serializer = GetUsersSerializer(user, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None):
+        user = self.get_user(pk)
+        serializer = CreateUserSerializer(user, many=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        user = self.get_user(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
