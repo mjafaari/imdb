5b15ab9393e80dcc354345c5869ab0ed497e3843
mjafaari <mjafaari99@gmail.com>
2022-03-10 21:45:54 +0330

    Movie, Genre and Director models added

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name= models.CharField(max_length=80, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Director(models.Model):
+    name= models.CharField(max_length=80, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    name = models.CharField(max_length=200, unique=True, primary_key=True)
+    director = models.ManyToManyField(Director)
+    Description = models.TextField(blank=True, null=True)
+    genre = models.ManyToManyField(Genre)
+    
+    def __str__(self):
+        return self.name
