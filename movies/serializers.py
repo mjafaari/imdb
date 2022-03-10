1699f41a02aff0de4bb4eefe0e53835a1fa0651e
mjafaari <mjafaari99@gmail.com>
2022-03-10 21:49:32 +0330

    serializers added

---
+++ movies/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from movies.models import Like, Movie, Comment, Genre, Director
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = '__all__'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = '__all__'
